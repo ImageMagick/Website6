c24d707b8037f98e6b9db900597f3b74d9f7bff0
Cristy <urban-warrior@imagemagick.org>
2019-09-28 15:38:34 -0400

    ...

--- include/define.php
+++ include/define.php
@@ -2,12 +2,12 @@
   /*
     ImageMagick Constants.
   */
-  define('DevelopmentTeam', 'development-team@imagemagick.org');
+  define('DevelopmentTeam', 'dirk@lemstra.org');
   define('SponsorIcon', 'networkredux.png');
   define('SponsorURL', 'http://www.networkredux.com');
   define('MagickMajorReleaseText', '6');
   define('MagickLibVersionText', '6.9.10');
-  define('MagickLibSubversion', '-23');
+  define('MagickLibSubversion', '-66');
   define('PerlMagickVersion', '6.9A');
-  define('MagickReleaseDate', '2019-01-03');
+  define('MagickReleaseDate', '2019-09-24');
 ?>
