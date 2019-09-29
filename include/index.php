7c16c444726d371add097bdacc04ed2e776677e4
Cristy <urban-warrior@imagemagick.org>
2019-09-29 10:28:09 -0400

    ...

--- include/index.php
+++ include/index.php
@@ -165,9 +165,9 @@ Use ImageMagick<sup><a href="http://tarr.uspto.gov/servlet/tarr?regser=serial&am
 <p>Want more performance from ImageMagick?  Try these options:</p>
 
 <ul>
-<li>Add more memory to your system, see <a href="https://legacy.imagemagick.org/script/architecture.php#cache">the pixel cache</a>;</li>
-<li>Add more cores to your system, see <a href="https://legacy.imagemagick.org/script/architecture.php#threads">threads of execution support</a>;</li>
-<li>Reduce lock contention with the <a href="http://goog-perftools.sourceforge.net/doc/tcmalloc.html">>tcmalloc</a> memory allocation library;</li>
+<li>add more memory to your system, see <a href="https://legacy.imagemagick.org/script/architecture.php#cache">the pixel cache</a>;</li>
+<li>add more cores to your system, see <a href="https://legacy.imagemagick.org/script/architecture.php#threads">threads of execution support</a>;</li>
+<li>reduce lock contention with the <a href="http://goog-perftools.sourceforge.net/doc/tcmalloc.html">tcmalloc</a> memory allocation library;</li>
 <li>push large images to a solid-state drive, see <a href="https://legacy.imagemagick.org/script/architecture.php#tera-pixel">large image support</a>.</li>
 </ul>
 <p>If these options are prohibitive, you can reduce the quality of the image results.  The default build is Q16.  If you instead use a Q8 build, you use half the memory The tradeoff is reduced precision.  For a Q8 build of ImageMagick, use this <code>configure</code> script option: <code>--with-quantum-depth=8</code>.</p>
