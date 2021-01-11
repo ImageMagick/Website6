2aac82bcebeef1ad28a5c97cc2cc75cd6c238615
Cristy <urban-warrior@imagemagick.org>
2021-01-11 13:18:41 +0000

    ...

---
+++ include/cite.php
@@ -0,0 +1,22 @@
+<div class="magick-header">
+<h1 class="text-center">Cite</h1>
+<p class="lead magick-description">Learn how to cite ImageMagick in your work.  Choose from these citation formats:</p>
+
+<h5>Citation in APA style</h5>
+<p class="highlight">The ImageMagick Development Team. (2021). ImageMagick. Retrieved from https://imagemagick.org</p>
+
+<h5>Citation in Vancouver style</h5>
+<p class="highlight">1. The ImageMagick Development Team. ImageMagick [Internet]. 2021. Available from: https://imagemagick.org</p>
+
+<h5>Citation in Harvard style</h5>
+<p class="highlight">The ImageMagick Development Team, 2021. ImageMagick, Available at: https://imagemagick.org.</p>
+<h5>Citation in Bibtex format</h5>
+<div class="highlight">
+@software{imagemagick,
+<br>&nbsp;&nbsp;author&nbsp;=&nbsp;{{The&nbsp;ImageMagick&nbsp;Development&nbsp;Team}},
+<br>&nbsp;&nbsp;title&nbsp;=&nbsp;{ImageMagick},
+<br>&nbsp;&nbsp;url&nbsp;=&nbsp;{https://imagemagick.org},
+<br>&nbsp;&nbsp;version&nbsp;=&nbsp;{7.0.10},
+<br>&nbsp;&nbsp;date&nbsp;=&nbsp;{2021-01-04},
+<br>}
+</div>
