54c5ac44e9541eaa54e25630c06fe16358aed3dc
Cristy <urban-warrior@imagemagick.org>
2021-01-31 13:56:10 +0000

    ...

--- include/identify.php
+++ include/identify.php
@@ -14,7 +14,7 @@ rose.jpg JPEG 70x46 70x46+0+0 8-bit sRGB 2.36KB 0.000u 0:00.000
 
 <p>By default, <code>identify</code> provides the following output:</p>
 ￼
-<p><code>Filename[frame #] image-format widthxheight page-widthxpage-height+x-offset+y-offset colorspace user-time elapsed-time</code></p>
+<ul><code>Filename[frame #] image-format widthxheight page-widthxpage-height+x-offset+y-offset colorspace user-time elapsed-time</code></ul>
 
 <p>Next, we look at the same image in greater detail:</p>
 
@@ -105,6 +105,10 @@ Image: rose.jpg
   Elapsed time: 0:01.000
   Version: ImageMagick Q16 https://imagemagick.org
 </code></pre>
+
+<p>Note, the image signature is generated from the pixel components, not the ima
+ge metadata.</p>
+
 
 <p>To get the print size in inches of an image at 72 DPI, use:</p>
 
