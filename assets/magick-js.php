1c417d2f59d73838a08e34f9c06c5847206ccc5b
Cristy <urban-warrior@imagemagick.org>
2021-02-03 14:38:36 +0000

    ...

--- assets/magick-js.php
+++ assets/magick-js.php
@@ -5,8 +5,6 @@
   header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
   header('Expires: ' . gmdate('D, d M Y H:i:s',time()+604800) . ' GMT');
   header("Content-type: text/javascript; charset=utf-8");
-  readfile('jquery-3.3.1.slim.min.js');
-  readfile('popper.min.js');
-  readfile('bootstrap.min.js');
+  readfile('bootstrap.bundle.min.js');
   ob_end_flush();
 ?>
