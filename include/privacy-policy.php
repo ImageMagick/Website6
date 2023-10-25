566f77a089dd0c3d0fc30c62157904e3ab5210e8
Cristy <urban-warrior@imagemagick.org>
2023-10-24 20:44:58 -0400

    ...

---
+++ include/privacy-policy.php
@@ -0,0 +1,38 @@
+<div class="magick-header">
+<h1 class="text-center">ImageMagick Privacy Policy</h1>
+
+<p>Welcome to the ImageMagick open-source project! This Privacy Policy is designed to help you understand how we collect, use, disclose, and safeguard your personal information when you interact with our software, website, and services.</p>
+
+
+<h3>1. Information We Collect</h3>
+<h4>1.1 Information You Provide</h4>
+<p>We do not collect personal information directly from users of ImageMagick. The software does not require you to create an account or provide personal details to access its functionality.</p>
+
+<h4>1.2 Information We Automatically Collect</h4>
+<p>ImageMagick may collect certain technical information about your use of the software, including but not limited to:</p>
+
+<p>Log information: We may collect server logs, including IP addresses, browser types, and operating systems, to help diagnose technical issues and improve the software.</p>
+<p>Usage data: ImageMagick may collect usage data, such as the commands and operations you perform within the software, but it does not tie this data to specific individuals.</p>
+<h3>2. How We Use Your Information</h3>
+<p>We use the information collected to:</p>
+
+<p>Improve ImageMagick: We may analyze usage data to identify and fix software bugs, enhance features, and optimize performance.</p>
+<p>Security: We monitor server logs to detect and prevent security threats.
+Compliance: We may use this information as necessary to comply with legal obligations.</p>
+<h3>3. Sharing Your Information</h3>
+<p>We do not share your personal information with third parties, except in the following cases:</p>
+
+<p>Legal Requirements: We may disclose information as required by law or to protect our legal rights.</p>
+<h3>4. Data Security</h3>
+<p>ImageMagick takes appropriate technical and organizational measures to protect the information we collect. While we do our best to secure your data, no method of transmission over the internet or electronic storage is 100% secure, and we cannot guarantee absolute security.</p>
+
+<h3>5. Your Choices</h3>
+<p>You have the option to use ImageMagick without providing personal information. You can control the data you share through your interaction with the software.</p>
+
+<h3>6. Changes to this Privacy Policy</h3>
+<p>This Privacy Policy may be updated from time to time. Any changes will be posted on this page with the "Last Updated" date. By continuing to use ImageMagick after such changes, you agree to the updated policy.</p>
+
+<h3>7. Contact Us</h3>
+<p>If you have any questions or concerns about this Privacy Policy or the way we handle your data, please <a href="https://github.com/ImageMagick/ImageMagick/discussions/categories/help">contact us</a>.</p>
+
+</div>
