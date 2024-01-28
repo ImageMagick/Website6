577c7783b26f7dc76bc85a321ed75ed555771937
Cristy <urban-warrior@imagemagick.org>
2024-01-27 20:57:42 -0500

    ...

--- include/links.php
+++ include/links.php
@@ -77,7 +77,7 @@
 
 <h2><a class="anchor" id="other-projects"></a>Other Projects</h2>
 <ul>
-  <dd class="col-md-8"><a href="https://urban-warrior.org/WizardsToolkit" taget="_blank">Wizard's Toolkit</a>
+  <dd class="col-md-8"><a href="https://urban-warrior.org" taget="_blank">Wizard's Toolkit</a>
   <dd class="col-md-8"><a href="https://github.com/cherryleafroad/Android-ImageMagick7" taget="_blank">ImageMagick API on the Android Platform</a></dd>
   <dd class="col-md-8"><a href="https://imagemagick.org/archive/iOS/" taget="_blank">ImageMagick API on the iOS / iPhone  Platform</a></dd>
   <dd class="col-md-8"><a href="https://spiritual-warrior.org/" taget="_blank">The Tao of the Spiritual Warrior</a></dd>
