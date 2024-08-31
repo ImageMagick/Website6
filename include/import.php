d58b96adf961edc590916d470670006ad8a533d4
Cristy <urban-warrior@imagemagick.org>
2024-08-31 09:12:03 -0400

    ...

--- include/import.php
+++ include/import.php
@@ -20,7 +20,7 @@
 
 <p>This command enables you to select a specific area on your screen and copy the selection to your clipboard. You can use this feature to conveniently post images on applications that support image uploading:</p>
 
-<pre class="p-3 mb-2 text-body-secondary bg-body-tertiary cli"><samp>import png:- | xclip -selection clipboard -t image/png</samp></pre>
+<pre class="p-3 mb-2 text-body-secondary bg-body-tertiary cli"><samp>import clipboard:</samp></pre>
 
 <p>You can find additional examples of using <code>import</code> in <a href="https://legacy.imagemagick.org/Usage/">Examples of ImageMagick Usage</a>.</p>
 
