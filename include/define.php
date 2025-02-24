dfac248fc7f37a41d9f254028d1de10bc3faba36
Dirk Lemstra <dirk@lemstra.org>
2025-02-24 21:22:02 +0100

    Added Magick prefix to all defines.

--- include/define.php
+++ include/define.php
@@ -2,7 +2,7 @@
   /*
     ImageMagick Constants.
   */
-  define('DevelopmentTeam', 'support-magick-0x01@urban-warrior.org');
+  define('MagickDevelopmentTeam', 'support-magick-0x01@urban-warrior.org');
   define('MagickMajorReleaseText', '6');
   define('MagickLibVersionText', '6.9.13');
   define('MagickLibSubversion', '-17');
