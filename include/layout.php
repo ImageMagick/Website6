c659fdf35902472c8bcad9843d53696af3dfd30c
Cristy <urban-warrior@imagemagick.org>
2025-07-31 08:40:45 -0400

    ...

--- include/layout.php
+++ include/layout.php
@@ -66,15 +66,6 @@
       setTheme(savedTheme);
     });
   </script>
-  <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
-    <symbol id="arrow-right-circle" viewBox="0 0 16 16">
-      <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-    </symbol>
-    <symbol id="color-mode" viewBox="0 0 118 94">
-      <title>Color Modes</title>
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z"></path>
-    </symbol>
-  </svg>
   <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
   <div class="container-fluid">
     <a class="navbar-brand" href="<?php echo $_SESSION['RelativePath']?>/../"><img class="d-block" id="icon" alt="ImageMagick" width="32" height="32" src="<?php echo $_SESSION['RelativePath']?>/../image/wand.ico"/></a>
@@ -83,7 +74,7 @@
     </button>
 
     <div class="collapse navbar-collapse" id="magick-navbars">
-      <ul class="navbar-nav me-auto mb-2 mb-md-0">
+      <ul class="navbar-nav me-auto">
         <li class="nav-item">
           <a class="nav-link <?php IsActive($topic,'Download'); ?>" href="<?php echo $_SESSION['RelativePath']?>/../script/download.php">Download</a>
         </li>
@@ -99,10 +90,18 @@
         <li class="nav-item">
           <a class="nav-link" rel="noopener" target="_blank" href="https://github.com/ImageMagick/ImageMagick6/discussions">Community</a>
         </li>
-        <li class="nav-item ms-3">
+        <li class="nav-item ms-4">
           <iframe src="https://github.com/sponsors/ImageMagick/button" title="Sponsor ImageMagick" height="35" width="107" style="border: 0;"></iframe>
         </li>
-        <li class="nav-item  ms-3 dropdown">
+      </ul>
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item dropdown">
+          <form class="d-flex form-inline" action="/script/search.php">
+            <input class="form-control me-2" type="text" name="q" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit" name="sa">Search</button>
+          </form>
+        </li>
+        <li class="nav-item dropdown ms-2">
           <a class="nav-link dropdown-toggle" href="#" id="themeDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             <span id="currentThemeIcon">🌓</span>
           </a>
@@ -113,10 +112,6 @@
           </ul>
         </li>
       </ul>
-      <form class="d-flex form-inline" action="/script/search.php">
-        <input class="form-control me-2" type="text" name="q" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit" name="sa">Search</button>
-      </form>
     </div>
   </div>
   </nav>
